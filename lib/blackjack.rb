241fedea72753893061893dcad2a3d08b48bb414
jaf7 <janthonyfields@gmail.com>
2018-01-17 13:54:28 -0500

    Built helper and runner methods for simple blackjack CLI

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,54 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  card_number = rand(1..11)
+  card_number
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
+  input
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card
+  card_total += deal_card
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(current_card_total)
+  input = get_user_input
+  
+  if input == "s"
+    prompt_user
+    current_card_total
+  elsif input == "h"
+    current_card_total += deal_card
+    current_card_total
+  else
+    invalid_command
+    prompt_user
+  end
+
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +56,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  current_card_total = initial_round
+
+  until current_card_total > 21 do
+    current_card_total = hit?(current_card_total)
+    prompt_user
+    display_card_total(current_card_total)
+  end
+
+  end_game(current_card_total)
 end
-    
